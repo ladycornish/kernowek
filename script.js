445634f04424d5ae36c3e6a37de7129de8f3fcbd
Mel <melcornish@me.com>
2021-10-08 13:30:10 +0100

    add KC svg so mask hopefully works & update styles to go with this

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ const navUL = document.getElementById('nav-ul');
 
 hamburger.addEventListener('click', () => {
     navUL.classList.toggle('show');
+
+    setTimeout(function () {
+        document.querySelector('nav-ul').style.display = 'none'
+    }, 5000)
+    setTimeout("location.reload(true);", 5000);
 })
+
+
